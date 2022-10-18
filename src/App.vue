a541e889fae066809eb5f0c4cded9a1d33416302
dmitrypeshnin <nexa0984@gmail.com>
2022-10-18 21:07:35 +0700

    Apply reset styles in Header.vue

--- src/App.vue
+++ src/App.vue
@@ -22,25 +22,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// Gilroy-Medium
-@font-face {
-	font-family: 'Gilroy-Medium';
-	src: url('./assets/fonts/gilroy-medium/gilroy-medium.eot'); /* IE 9 Compatibility Mode */
-	src: url('./assets/fonts/gilroy-medium/gilroy-medium.eot?#iefix') format('embedded-opentype'), /* IE < 9 */
-		url('./assets/fonts/gilroy-medium/gilroy-medium.woff2') format('woff2'), /* Super Modern Browsers */
-		url('./assets/fonts/gilroy-medium/gilroy-medium.woff') format('woff'), /* Firefox >= 3.6, any other modern browser */
-		url('./assets/fonts/gilroy-medium/gilroy-medium.ttf') format('truetype'), /* Safari, Android, iOS */
-		url('./assets/fonts/gilroy-medium/gilroy-medium.svg#gilroy-medium') format('svg'); /* Chrome < 4, Legacy iOS */
-}
+  // Gilroy-Medium font
+  @font-face {
+    font-family: 'Gilroy-Medium';
+    src: url('./assets/fonts/gilroy-medium/gilroy-medium.eot'); /* IE 9 Compatibility Mode */
+    src: url('./assets/fonts/gilroy-medium/gilroy-medium.eot?#iefix') format('embedded-opentype'), /* IE < 9 */
+      url('./assets/fonts/gilroy-medium/gilroy-medium.woff2') format('woff2'), /* Super Modern Browsers */
+      url('./assets/fonts/gilroy-medium/gilroy-medium.woff') format('woff'), /* Firefox >= 3.6, any other modern browser */
+      url('./assets/fonts/gilroy-medium/gilroy-medium.ttf') format('truetype'), /* Safari, Android, iOS */
+      url('./assets/fonts/gilroy-medium/gilroy-medium.svg#gilroy-medium') format('svg'); /* Chrome < 4, Legacy iOS */
+  }
 
-// Gilroy-Bold
-@font-face {
-	font-family: 'Gilroy-Bold';
-	src: url('./assets/fonts/gilroy-bold/gilroy-bold.eot'); /* IE 9 Compatibility Mode */
-	src: url('./assets/fonts/gilroy-bold/gilroy-bold.eot?#iefix') format('embedded-opentype'), /* IE < 9 */
-		url('./assets/fonts/gilroy-bold/gilroy-bold.woff2') format('woff2'), /* Super Modern Browsers */
-		url('./assets/fonts/gilroy-bold/gilroy-bold.woff') format('woff'), /* Firefox >= 3.6, any other modern browser */
-		url('./assets/fonts/gilroy-bold/gilroy-bold.ttf') format('truetype'), /* Safari, Android, iOS */
-		url('./assets/fonts/gilroy-bold/gilroy-bold.svg#gilroy-bold') format('svg'); /* Chrome < 4, Legacy iOS */
-}
+  // Gilroy-Bold font
+  @font-face {
+    font-family: 'Gilroy-Bold';
+    src: url('./assets/fonts/gilroy-bold/gilroy-bold.eot'); /* IE 9 Compatibility Mode */
+    src: url('./assets/fonts/gilroy-bold/gilroy-bold.eot?#iefix') format('embedded-opentype'), /* IE < 9 */
+      url('./assets/fonts/gilroy-bold/gilroy-bold.woff2') format('woff2'), /* Super Modern Browsers */
+      url('./assets/fonts/gilroy-bold/gilroy-bold.woff') format('woff'), /* Firefox >= 3.6, any other modern browser */
+      url('./assets/fonts/gilroy-bold/gilroy-bold.ttf') format('truetype'), /* Safari, Android, iOS */
+      url('./assets/fonts/gilroy-bold/gilroy-bold.svg#gilroy-bold') format('svg'); /* Chrome < 4, Legacy iOS */
+  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,34 @@
 <template>
-  <h1>Denis Novik</h1>
+  <header class="header">
+    <h1>Dmitriy Novik</h1>
+    <div class="container">
+      <nav class="header__menu menu">
+        <div class="menu__items">
+          <a href="" class="menu__link" v-for="item in items" :key="item">{{ item }}</a>
+        </div>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
   name: "HeaderBlock",
+  data() {
+    return {
+      items: ["Home", "About me", "Skills", "Portfolio", "Contacts" ]
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  font-family: "Gilroy-Bold";
-  font-size: 47.3392px;
-  line-height: 59px;
-  color: #070707;
-}
+  // style reset
+  @import '../../node_modules/css-reset-and-normalize/scss/reset-and-normalize.scss';
+  @import '../../node_modules/css-reset-and-normalize/scss/link-reset.scss';
+  @import '../../node_modules/css-reset-and-normalize/scss/button-reset.scss';
+  
+  .container {
+    max-width: 945px;
+  }
 </style>
