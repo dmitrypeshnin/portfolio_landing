25f7ba268e8e5c985304e55ddd9c0b708edd218e
dmitrypeshnin <nexa0984@gmail.com>
2022-11-18 09:55:41 +0700

    Added SkillsBlock, PortfolioBlock and ContactsBlock

---
+++ src/components/ContactsBlock.vue
@@ -0,0 +1,110 @@
+<template>
+    <footer class="contactsBlock">
+        <h2 class="contactsBlock__title">Contacts</h2>
+        <p class="contactsBlock__subtitle">Want to know more or just chat? You are welcome!</p>
+        <div class="contactsBlock__button">Send message</div>
+        <ul class="contactsBlock__social">
+            <li class="contactsBlock__social-icon">
+                <a class="contactsBlock_social-link" href="#">
+                    <img class="contactsBlock__social-icon" src="../assets/img/linkedin-icon.svg" alt="LinkedIn">
+                </a>
+            </li>
+            <li class="contactsBlock__social-icon">
+                <a class="contactsBlock_social-link" href="#">
+                    <img class="contactsBlock__social-icon" src="../assets/img/instagram-icon.svg" alt="Instagram">
+                </a>
+            </li>
+            <li class="contactsBlock__social-icon">
+                <a class="contactsBlock_social-link" href="#">
+                    <img class="contactsBlock__social-icon" src="../assets/img/behance.svg" alt="Behance">
+                </a>
+            </li>
+            <li class="contactsBlock__social-icon">
+                <a class="contactsBlock_social-link" href="#">
+                    <img class="contactsBlock__social-icon" src="../assets/img/dribble.svg" alt="Behance">
+                </a>
+            </li>
+        </ul>
+        <div class="contactsBlock__text">
+            Like me on<br>
+            LinkedIn, Instagram, Behance, Dribble
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: "ContactsBlock"
+};
+</script>
+
+<style lang="scss" scoped>
+    .contactsBlock {
+        padding-top: 111px;
+        padding-bottom: 123px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        &__title {
+            font-family: 'Gilroy-Bold';
+            font-size: 34px;
+            line-height: 42px;
+            color: #070707;
+            margin-bottom: 20px;
+        }
+        &__subtitle {
+            max-width: 275px;
+            font-family: 'Gilroy-Medium';
+            font-size: 18px;
+            line-height: 22px;
+            text-align: center;
+            color: #070707;
+            margin: 0 auto;
+            margin-bottom: 30px;
+        }
+        &__button {
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 180px;
+            height: 42px;
+            border-radius: 50px;
+            background: #070707;
+            font-family: 'Gilroy-Bold';
+            font-size: 18px;
+            line-height: 22px;
+            text-align: center;
+            color: #FFFFFF;
+            margin-bottom: 85px;
+            cursor: pointer;
+            &::after {
+                content: '';
+                position: absolute;
+                width: 166px;
+                height: 38px;
+                left: 8px;
+                top: 10px;
+                opacity: 0.1;
+                filter: blur(10px);
+            }
+        }
+        &__social {
+            max-width: 342px;
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 45px;
+        }
+        &__social {
+            width: 342px;
+        }
+        &__text {
+            font-family: 'Gilroy-Medium';
+            font-size: 14px;
+            line-height: 17px;
+            text-align: center;
+            color: #828282;
+        }
+    }
+
+</style>
---
+++ src/components/SkillsBlock.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="skillsBlock">
+      <div class="skillsBlock__inner">
+        <h2 class="skillsBlock__title">Skills</h2>
+        <p class="skillsBlock__text">I work in such programs as</p>
+        <div class="skillsBlock__program-levels program-levels">
+          <div class="program-levels__item">
+            <img class="program-levels__app-icon" src="../assets/img/photoshop-icon.png" alt="photoshop-icon">
+            <h3 class="program-levels__app-title">Adobe<br>Photoshop</h3>
+            <div class="program-levels__stars stars">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/light-star.png" alt="light-star">
+            </div>
+          </div>
+          <div class="program-levels__item">
+            <img class="program-levels__app-icon" src="../assets/img/illustrator-icon.png" alt="illustrator-icon">
+            <h3 class="program-levels__app-title">Adobe<br>Illustrator</h3>
+            <div class="program-levels__stars stars">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/light-star.png" alt="light-star">
+              <img class="stars__star-item" src="../assets/img/light-star.png" alt="light-star">
+            </div>
+          </div>
+          <div class="program-levels__item">
+            <img class="program-levels__app-icon" src="../assets/img/after-effects-icon.png" alt="after-effects-icon">
+            <h3 class="program-levels__app-title">Adobe<br>After Effects</h3>
+            <div class="program-levels__stars stars">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/light-star.png" alt="light-star">
+            </div>
+          </div>
+          <div class="program-levels__item">
+            <img class="program-levels__app-icon" src="../assets/img/figma-icon.png" alt="figma-icon">
+            <h3 class="program-levels__app-title">Figma</h3>
+            <div class="program-levels__stars stars">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/dark-star.png" alt="dark-star">
+              <img class="stars__star-item" src="../assets/img/light-star.png" alt="light-star">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "SkillsBlock"
+};
+</script>
+
+<style lang="scss" scoped>
+  .skillsBlock {
+    &__inner {
+      max-width: 935px;
+      height: 610px;
+      padding-top: 91px;
+      margin: 0 auto;
+    }
+    &__title {
+      text-align: center;
+      margin-bottom: 70px;
+      font-family: 'Gilroy-Bold';
+      font-size: 34px;
+      line-height: 42px;
+      color: #070707;
+    }
+    &__text {
+      text-align: center;
+      margin-bottom: 83px;
+      font-family: 'Gilroy-Medium';
+      font-size: 18px;
+      line-height: 22px;
+      color: #070707;
+    }
+  }
+
+  .program-levels {
+    display: flex;
+    justify-content: space-between;
+    
+    &__item {
+      width: 166px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+    }
+    &__app-icon {
+      height: 90px;
+      margin-bottom: 24px;
+    }
+    &__app-title {
+      height: 27px;
+      margin-bottom: 40px;
+      text-align: center;
+      font-family: 'Gilroy-Bold';
+      font-size: 14px;
+      line-height: 17px;
+      color: #828282;
+    }
+  }
+
+  .stars {
+    min-width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
